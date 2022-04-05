7e946c75ae0773a5f25a5af7918f26dd031b0b02
Adilarshad459 <102904610+Adilarshad459@users.noreply.github.com>
2022-04-05 03:04:20 -0700

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+	string firstname, lastname;
+	cout << "Please Enter your First Name: ";
+	cin>> firstname;
+	cout <<"Please Enter your Last Name: ";
+	cin >> lastname;
+	cout << "Your good name is: "<<firstname <<" " <<lastname ; 
+	return 0;
+	
+}
